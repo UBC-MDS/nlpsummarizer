b738e8b6e5872015199279d001f3545f70b4c33f
Karanpal Singh <karanpal.singh@alumni.ubc.ca>
2020-02-26 18:21:45 -0800

    polarity description

---
+++ polarity.R
@@ -0,0 +1,28 @@
+# author : Karanpal Singh
+# date : 26 Feb 2020
+
+#' Polarity
+#'   This method will check and compute the polarity of the text data.
+#'   This method will return:
+#'   - Number of Positive words
+#'   - Number of Negative words
+#'   - Number of Neutral words
+#'
+#' @param df$col
+#' This method will expect dataframe's column with textual data
+#'
+#' @return
+#' nop(integer): number of positive words
+#' nong(integer): number of negative words
+#' non(integer): number of neutral words
+#'
+#' @examples
+#'     >>>> df = pd.DataFrame({'text': 'He is a good guy.
+#'                              This is the worst coffee I had in my life.'})
+#'    >>>> polarity(df['text'])
+#'    [1]  | positive words | negative words | neutral words |
+#'         |         1      |           1    |    0          |
+
+polarity <- function(df_col){
+
+}
