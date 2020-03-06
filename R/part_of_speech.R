1248e9eeda833785cff79f478595da35913c97f9
Karlos Muradyan <carlosmuradyan2@gmail.com>
2020-03-06 12:31:15 -0800

    Implemented part_of_speech function

--- R/part_of_speech.R
+++ R/part_of_speech.R
@@ -1,6 +1,7 @@
 # author : Karlos Muradyan 
 # date : 26 Feb 2020
 
+library(udpipe)
 
 #' This function generates statistics about the proportions of following
 #' parts of speech in the given column:
@@ -22,6 +23,23 @@
 #'      [1]  |   verbs    | prepositions | adjectives |   nouns   |  articles  |
 #'           |    0.2     |     0.11     |     0.3    |    0.06   |     0.18   |
 
-get_part_of_speech <- function(df_col){
+get_part_of_speech <- function(df_col, model_path = './data/english-ewt-ud-2.4-190531.udpipe'){
+	if (!file.exists(model_path)){
+		print('Model does not exist in ./data folder')
+		print('Downloading...')
+		model_path <- udpipe_download_model(language = "english", model_dir = './data')$file_model
+	}
+	udmodel <- udpipe_load_model(file = model_path)
+	tagged_text <- udpipe_annotate(udmodel, df_col)
+	resulting_df <- as.data.frame(tagged_text)
 
+	proportions <- table(resulting_df$upos)/nrow(resulting_df)
+	results <- as.data.frame(t(as.matrix(data.frame(proportions))))
+	names(results) <- as.character(unlist(results[1,]))
+	results <- results[-1,]
+	return(results)
 }
+
+ex <- data.frame(text_col =  c('Today is a beautiful Monday'))
+print(get_part_of_speech(ex$text_col))
+
